49c36ef2a7e2e333dda92b2692097e2769f32fc0
Grach <egri4stk@gmail.com>
2016-04-29 20:00:43 +0300

    delete trash

--- Chat/run-client-windows.cmd
+++
@@ -1,1 +0,0 @@
-"%JAVA_HOME%/bin/java" -cp bin;libs/json-simple-1.1.1.jar by.bsu.up.chat.Launcher --client -l 192.168.0.101 -p 999
--- Chat/src/by/bsu/up/chat/utils/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package by.bsu.up.chat.utils;
-
-public class StringUtils {
-
-    public static boolean isEmpty(String str) {
-        return str == null || str.isEmpty();
-    }
-}
